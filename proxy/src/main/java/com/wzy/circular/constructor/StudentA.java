de49dfe2df86f5b71d0e17127494ce778554f8cc
wuzhaoyu <1966643318@qq.com>
2020-12-22 09:41:33 +0800

    循环依赖

---
+++ proxy/src/main/java/com/wzy/circular/constructor/StudentA.java
@@ -0,0 +1,20 @@
+package com.wzy.circular.constructor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+/**
+ * 类功能说明:
+ * 类修改者	创建日期2020/12/21
+ * 修改说明
+ *
+ * @author wzy
+ * @version V1.0
+ * @description 说明：
+ **/
+@Service
+public class StudentA {
+
+    public StudentA(StudentB studentB) {
+    }
+}
---
+++ proxy/src/main/java/com/wzy/circular/field/StudentA.java
@@ -0,0 +1,21 @@
+package com.wzy.circular.field;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * 类功能说明:
+ * 类修改者	创建日期2020/12/21
+ * 修改说明
+ *
+ * @author wzy
+ * @version V1.0
+ * @description 说明：
+ **/
+@Component
+public class StudentA {
+
+    @Autowired
+    private StudentB studentB ;
+
+}
